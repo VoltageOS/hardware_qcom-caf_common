482c4ec55aa3d9640196eba1b2511bfd0b2441de
Pig <pig.priv@gmail.com>
2023-10-29 05:57:33 +0000

    os_pickup_sepolicy_vndr: Include VoltageOS-specific QCOM sepolicy
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: Ibf70e4c8ab9d91b50c62c3e9f1263e1624e8ca00

--- os_pickup_sepolicy_vndr.mk
+++ os_pickup_sepolicy_vndr.mk
@@ -10,4 +10,4 @@ else ifneq ($(filter $(UM_6_1_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     include device/qcom/sepolicy_vndr/sm8650/SEPolicy.mk
 endif
 
--include device/lineage/sepolicy/qcom/sepolicy.mk
+-include device/voltage/sepolicy/qcom/sepolicy.mk
